4b8ce1983784d24a84fe1e98db390c4fb9490bb6
Kwan Noel <noelkwan1998@gmail.com>
2020-11-07 18:36:36 +0800

    Implement freshN, allows functions with variable arguments

--- miniKanren.hs
+++ miniKanren.hs
@@ -1,3 +1,6 @@
+{-# LANGUAGE FlexibleInstances     #-}
+{-# LANGUAGE MultiParamTypeClasses #-}
+{-# LANGUAGE TypeSynonymInstances  #-}
 {-|
 =========
 == WIP ==
@@ -6,23 +9,44 @@
 
 module MiniKanren where
 
-import           MicroKanren (Goal, State, Stream (..), Subst, Term (..), Var,
-                              VariableCounter, conj, delay, disj, extS, fresh,
-                              unify, walk, (===))
+import           MicroKanren        (Goal, State, Stream (..), Subst, Term (..),
+                                     Var, VariableCounter, conj, delay, disj,
+                                     extS, fresh, unify, walk, (===))
 
--- | Disj' provides us a way to safely disj goals which are recursive
-disj' :: Goal -> Goal -> Goal
-disj' g1 g2 = delay $ disj g1 g2
+import           Data.List.NonEmpty (NonEmpty (..))
+import qualified Data.List.NonEmpty as NE
 
--- | Conj' provides us a way to safely conj goals which are recursive
-conj' :: Goal -> Goal -> Goal
-conj' g1 g2 = delay $ conj g1 g2
+-- | Safe disj of one or more goals
+disjN :: NonEmpty Goal -> Goal
+disjN = delay . foldr1 disj
 
--- | Conde - satisfy list of goals
-conde :: [Goal] -> Goal
-conde = foldr conj' $ const Nil
+-- | Safe conj of one or more goals
+conjN :: NonEmpty Goal -> Goal
+conjN = delay . foldr1 conj
 
--- | Fresh'
+-- | Conde -> disjunction of a series of conjunctions
+-- disjN [ conjN [g1, g2, ...]
+--       , conjN [h1, h2, ...]
+--       ]
+conde :: NonEmpty (NonEmpty Goal) -> Goal
+conde = delay . disjN . fmap conjN
+
+-- | freshN -> takes in a function which performs binding of n variables to a goal
+freshN :: MultiParamFunction i r => (Term -> i -> r) -> Goal
+freshN f = \(s, c) -> let (c', g) = apply f c
+                      in g (s, c')
+
+class MultiParamFunction i r where
+    apply :: (Term -> i -> r) -> VariableCounter -> (VariableCounter, Goal)
+
+instance MultiParamFunction i r => MultiParamFunction Term (i -> r) where
+    apply f c = apply f' c'
+        where f' = f (Var c)
+              c' = c + 1
+
+-- | Base case
+instance MultiParamFunction State (Stream State) where
+    apply f c = (c + 1, f (Var c))
 
 main :: IO ()
 main = do
